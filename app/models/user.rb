35daf4f4d3b7216e0d5632de3fb353e4b4d9fdd6
kasi-iyo <snakeman143@gmail.com>
2020-08-30 17:41:34 +0900

    Userモデルのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :nickname, presence: true
+    validates :nickname, uniqueness: true
+    validates :nickname, length: { maximum: 30 }
+
+    validates :account_id, presence: true
+    validates :account_id, uniqueness: true
+    validates :accounr_id, length: { maximum: 15 }
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
+
 end
