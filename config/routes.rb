14566f2f1e395361e2a63b1bc56afc21891ba961
kasi-iyo <snakeman143@gmail.com>
2020-08-30 18:25:47 +0900

    Sessionsコントローラへのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   
   resources :users, only[:create, :show, :index]
 
+  post '/login', to: 'sessions#login'
+  delete '/logout', to: 'sessions#logout'
+  get '/logged_in', to: 'sessions#is_logged_in?'
 end
