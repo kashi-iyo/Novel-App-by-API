8355e1b974bf1fae948bf79808550fb5b0dcb398
kasi-iyo <snakeman143@gmail.com>
2020-09-02 16:59:16 +0900

    Api::V1::NovelSeriesコントローラへのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   
+  root 'series#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :novel_series, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
   resources :users, only: [:create, :show, :index]
 
   post '/login', to: 'sessions#login'
