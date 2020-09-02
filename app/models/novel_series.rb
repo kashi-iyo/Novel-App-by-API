544ff2be142c9a00df1e26012bedca8e453d97d6
kasi-iyo <snakeman143@gmail.com>
2020-09-02 16:57:58 +0900

    NovelSeriesモデルのバリデーション等。 Userモデルとの紐付け。

--- app/models/novel_series.rb
+++ app/models/novel_series.rb
@@ -1,3 +1,10 @@
 class NovelSeries < ApplicationRecord
+  default_scope { order(created_at: :desc) }
+
   belongs_to :user
+
+  validates :series_title, presence: true
+  validates :series_title, length: { maximum: 50 }
+
+  validates :series_description, length: { maximum: 300 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@ class User < ApplicationRecord
     validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
 
     validates :profile, length: { maximum: 200 }
+
+
+    # 紐付け====================================
+    has_many :novel_series, dependent: :destroy
+
 end
