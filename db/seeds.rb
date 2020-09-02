399ac724e699f532fa84812a11f824c9ca9df1c2
kasi-iyo <snakeman143@gmail.com>
2020-09-02 16:58:25 +0900

    NovelSeriesのSeedを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.all.each do |u|
+    10.times do |i|
+        u.novel_series.create(series_title: "シリーズ #{i+1} ")
+    end
+end
