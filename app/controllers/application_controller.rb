aaf8ef7148a990b5c7b9d567d31abeb236e79db5
kasi-iyo <snakeman143@gmail.com>
2020-09-02 18:53:27 +0900

    Applicationコントローラに、認証系のヘルパーメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,26 @@ class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
 
     # 認証系======================================================================
-    helper_method :login!, :logged_in?, :current_user, :authorized_user?
+    helper_method :login!, :logged_in?, :logged_in_user, :current_user
 
+    # ログインさせる
     def login!
         session[:user_id] = @user.id
     end
     
+    # ログインしているかどうかをbool値で返す
     def logged_in?
         !!session[:user_id]
     end
 
+    # ユーザーがログインしていない場合の処理
+    def logged_in_user
+        unless logged_in?
+            render json: { messages: "ログインまたは、新規登録を行ってください。", status: 401 }
+        end
+    end
+
+    # 現在ログインしているユーザーを返す
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
