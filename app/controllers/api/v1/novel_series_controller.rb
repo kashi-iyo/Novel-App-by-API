e093fd72bf98d46d5fe1a6fdbbac764afeefcc8f
kasi-iyo <snakeman143@gmail.com>
2020-09-02 19:33:05 +0900

    NovelSeriesコントローラ編集（createアクション、その他のプライベートメソッド追加）

--- app/controllers/api/v1/novel_series_controller.rb
+++ app/controllers/api/v1/novel_series_controller.rb
@@ -1,18 +1,43 @@
 class Api::V1::NovelSeriesController < ApplicationController
 
+    before_action :logged_in_user , only: [:create, :edit, :update, :destroy]
     before_action :set_novel_series, only: [:show, :edit, :update, :destroy]
 
     def index
         @all_novel_series = NovelSeries.all
-        render json: { status: "ok" , novel_series: @all_novel_series}
+        render json: { status: 200, novel_series: @all_novel_series}
     end
 
     def show
-        
+        render json: { status: 200, novel_series: @novel_series }
+    end
+
+    def edit
+        if authorized?
+            render json: { status: 200, novel_series: @novel_series }
+        else
+            handle_unauthorized
+        end
     end
 
     def create
-        
+        @novel_series = current_user.novel_series.new(novel_series_params)
+        if authorized?
+            if @novel_series.save
+                render json: {
+                    status: :created,
+                    novel_series: @novel_series,
+                    location: api_v1_novel_series_path(@novel_series)
+                }
+            else
+                render json: {
+                    errors: @novel_series.errors.full_messages,
+                    status: :unprocessable_entity
+                }
+            end
+        else
+            handle_unauthorized
+        end
     end
 
     def update
@@ -25,8 +50,25 @@ class Api::V1::NovelSeriesController < ApplicationController
 
     private
 
+        # シリーズを取得
         def set_novel_series
             @novel_series = NovelSeries.find(params[:id])
+        end
+
+        # ログイン中のユーザーと、今見ているシリーズの作成者が一致するかをbool値で返す
+        def authorized?
+            @novel_series.user == current_user
+        end
+
+        # ユーザー同士が不一致な場合の処理
+        def handle_unauthorized
+            unless authorized?
+                render json: { messages: "アクセス権限がありません。", status: 401 }
+            end
+        end
+
+        def novel_series_params
+            params.require(:novel_series).permit(:series_title, :series_description)
         end
 
 end
