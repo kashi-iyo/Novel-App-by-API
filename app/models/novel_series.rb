835e45725480ea6d20191c4d2d0120fe5942b1b2
kasi-iyo <snakeman143@gmail.com>
2020-09-08 18:27:03 +0900

    NovelSeriesモデル編集

--- app/models/novel_series.rb
+++ app/models/novel_series.rb
@@ -1,10 +1,16 @@
 class NovelSeries < ApplicationRecord
+  # 並び替え
   default_scope { order(created_at: :desc) }
 
+  # 紐付け
   belongs_to :user
+  has_many :novels, dependent: :destroy
 
+  # バリデーション
   validates :series_title, presence: true
+  validates :series_title, uniquness: true
   validates :series_title, length: { maximum: 50 }
-
   validates :series_description, length: { maximum: 300 }
+  validates :author, presence: true
+  validates :release, presence: true
 end
