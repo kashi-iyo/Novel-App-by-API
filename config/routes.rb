a4dc858bc06f265c6794f7c4c1c5b893cc155ace
kasi-iyo <snakeman143@gmail.com>
2020-09-10 12:22:42 +0900

    Novelsコントローラへのルーティングを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :novel_series, only: [:index, :show, :edit, :create, :update, :destroy]
+      resources :novel_series, only: [:index, :show, :edit, :create, :update, :destroy] do
+        resources :novels, only: [:index, :show, :edit, :create, :update, :destroy]
+      end
     end
   end
 
