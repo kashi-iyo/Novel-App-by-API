6ddada056ca619152a8c88d2ab09e62a762f5e28
kasi-iyo <snakeman143@gmail.com>
2020-09-27 19:02:37 +0900

    NovelSeriesを編集 →NovelTagMap, NovelTagとの関連づけ

--- app/models/novel_series.rb
+++ app/models/novel_series.rb
@@ -5,6 +5,8 @@ class NovelSeries < ApplicationRecord
   # 紐付け
   belongs_to :user
   has_many :novels, dependent: :destroy
+  has_many :novel_tag_maps, dependent: :destroy
+  has_many :novel_tags, through: :novel_tag_maps
 
   # バリデーション
   validates :series_title, presence: true
@@ -12,4 +14,27 @@ class NovelSeries < ApplicationRecord
   validates :series_title, length: { maximum: 50 }
   validates :series_description, length: { maximum: 300 }
   validates :author, presence: true
+
+  # インスタンスメソッド
+  def save_tag(sent_tags)
+    current_tags = self.novel_tags.pluck(:novel_tag_name) unless self.novel_tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+
+    old_tags.each do |old|
+      self.novel_tags.delete NovelTag.find_by(novel_tag_name: old)
+    end
+
+    new_tags.each do |new|
+      new_novel_tag = NovelTag.find_or_create_by(novel_tag_name: new)
+      self.novel_tags << new_novel_tag
+    end
+  end
+
+  # def self.push_novel_count
+  #   NovelSeries.each do |series|
+  #     novel = Novel.find_by(novel_series_id: series.id)
+  #     self << novel
+  #   end
+  # end
 end
