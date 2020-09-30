88d062d535a7aeeb0d795fdd87f869518dea2850
kasi-iyo <snakeman143@gmail.com>
2020-09-30 23:05:31 +0900

    series_tagsアクションへのルーティング追加 NovelSeriesコントローラ編集 →series_tagsアクション内の編集 　→シリーズID、シリーズが所有するタグを取得

--- app/controllers/api/v1/novel_series_controller.rb
+++ app/controllers/api/v1/novel_series_controller.rb
@@ -10,26 +10,34 @@ class Api::V1::NovelSeriesController < ApplicationController
         @all_novel_series.count_in_series(@all_novel_series)
         # @tags_in_series = @all_novel_series.tags_in_series(@all_novel_series)
         @series_count = @all_novel_series.count.to_s
-        # @tags_in_series = @all_novel_series.map{ |series|
+        # tags_in_series = @all_novel_series.map{ |series|
         #     [series.id, series.novel_tags.all]
         # }.to_h
+        # @tags_in_series = tags_in_series.map{ |key, tags|
+        #     ["series_id": key, "tags": tags]
+        # }
         render json: {
             status: 200,
-            tags: @tags,
-            # tags_in_series: @tags_in_series,
-            novel_series: @all_novel_series,
             series_count: @series_count,
+            tags: @tags,
+            # tags_in_series: @tags_in_series.flatten,
+            novel_series: @all_novel_series,
             keyword: "index_of_series"
         }
     end
 
     def series_tags
-        @series_tags = @novel_series.novel_tags.all.map do |tag|
-            unless tag === []
-                ["tag_name": tag["novel_tag_name"]]
-            end
-        end
-        render json: {status: 200, series_tags: @series_tags.flatten, keyword: "series_tags"}
+        series_tags = @novel_series.novel_tags
+        @series_tags = series_tags.map{ |tags|
+            [tags]
+        }
+        @id = @novel_series.id
+        render json: {
+            status: 200,
+            series_id: @id.to_s,
+            series_tags: @series_tags.flatten,
+            keyword: "series_tags"
+        }
     end
 
     def show
