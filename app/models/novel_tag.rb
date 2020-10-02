13626cb5b56c7816d3ddc19b150e2e34a8ba43b6
kasi-iyo <snakeman143@gmail.com>
2020-10-02 23:02:35 +0900

    NovelTagモデルを編集 →NovelTagモデルとNovelSeriesモデルを関連付ける

--- app/models/novel_tag.rb
+++ app/models/novel_tag.rb
@@ -1,3 +1,8 @@
 class NovelTag < ApplicationRecord
-    
+
+    validates :novel_tag_name, presence: true
+    # validates :novel_tag_name, uniquness: true
+
+    has_many :novel_tag_maps, dependent: :destroy, foreign_key: :novel_tag_id
+    has_many :novel_series, through: :novel_tag_maps
 end
