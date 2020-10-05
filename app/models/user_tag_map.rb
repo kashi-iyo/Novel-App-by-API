b499c1ab135144a249daac31becef619389e9872
kasi-iyo <snakeman143@gmail.com>
2020-10-05 20:33:52 +0900

    UserTagMapモデルを編集 →ユーザー用のタグは5個までなので、そのためのメソッドを定義

--- app/models/user_tag_map.rb
+++ app/models/user_tag_map.rb
@@ -1,4 +1,11 @@
 class UserTagMap < ApplicationRecord
   belongs_to :user
   belongs_to :user_tag
+
+  MAX_TAGS_COUNT = 5
+  validate :tags_count_limit
+  private
+    def tags_count_limit
+      errors.add(:base, "追加できる趣味タグは #{MAX_TAGS_COUNT}個 までです。") if user.user_tag_maps.count >= MAX_TAGS_COUNT
+    end
 end
