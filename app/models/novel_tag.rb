e0abef1f521d17c0122a14149137e8ee773f8457
kasi-iyo <snakeman143@gmail.com>
2020-10-06 21:13:58 +0900

    NovelTagモデルを編集 →tag_has_series_countメソッドを定義 　　→1つのタグを登録しているシリーズの数を取得

--- app/models/novel_tag.rb
+++ app/models/novel_tag.rb
@@ -5,4 +5,19 @@ class NovelTag < ApplicationRecord
 
     has_many :novel_tag_maps, dependent: :destroy, foreign_key: :novel_tag_id
     has_many :novel_series, through: :novel_tag_maps
+
+    # そのタグを登録しているシリーズの数
+    def self.tag_has_series_count(tags)
+        count = tags.map {|tag|
+            [tag.id, tag.novel_series.count.to_s]
+        }.to_h
+        count.map do |k, v|
+            tags.map do |tag|
+                if tag.id === k
+                    tag["count"] = v
+                end
+            end
+        end
+    end
+
 end
