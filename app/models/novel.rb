71450a70c00bc94dce5ce024f031a30f69ecf5c3
kasi-iyo <snakeman143@gmail.com>
2020-10-11 01:07:20 +0900

    Novelモデル微編集 →アソシエーション、バリデーションを修正

--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,8 +1,13 @@
 class Novel < ApplicationRecord
   # 紐付け
+  # ユーザー
   belongs_to :user
+  # シリーズ
   belongs_to :novel_series
-  has_many :novel_favorites
+  # お気に入り
+  has_many :novel_favorites, dependent: :destroy
+  # コメント
+  has_many :comments, dependent: :destroy
 
   # バリデーション
   validates :novel_title, presence: true
@@ -10,6 +15,8 @@ class Novel < ApplicationRecord
   validates :novel_description, length: { maximum: 300 }
   validates :novel_content, presence: true
   validates :author, presence: true
+  validates :user_id, presence: true
+  validates :novel_series_id, presence: true
 
   # ユーザーがその小説をお気に入りしているかどうかをチェック
   def favorited_by?(current_user)
