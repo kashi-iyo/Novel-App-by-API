9cfff8dec14f01280b0e1527c1416d6e6e79fb6a
kasi-iyo <snakeman143@gmail.com>
2020-10-11 23:40:55 +0900

    Commentsコントローラを編集 →edit, updateを

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::CommentsController < ApplicationController
 
-    before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
-    before_action :set_novel, only: [:index, :edit]
-    before_action :set_comment, only: [:edit, :update, :destroy]
+    before_action :logged_in_user, only: [:create, :destroy]
+    before_action :set_novel, only: [:index]
+    before_action :set_comment, only: [:destroy]
 
     def index
         @comments = @novel.comments
@@ -17,37 +17,13 @@ class Api::V1::CommentsController < ApplicationController
         @comment = current_user.comments.new(comment_params)
         if authorized?(@comment)
             if @comment.save
+                @novel = Novel.find_by(id: params[:novel_id])
+                @comments = @novel.comments
                 render json: {
                     status: :created,
-                    successful: ["正常に作成されました。"],
+                    comments: @comments,
+                    successful: ["正常に送信されました。"],
                     keyword: "create_comment"
-                }
-            else
-                render json: {
-                    errors: ["入力内容に誤りがあります。"],
-                    status: :unprocessable_entity
-                }
-            end
-        else
-            handle_unauthorized(@comment)
-        end
-    end
-
-    def edit
-        if authorized?(@comment)
-            render json: {status: 200, comment: @comment, keyword: "edit_comment"}
-        else
-            handle_unauthorized(@comment)
-        end
-    end
-
-    def update
-        if authorized?(@comment)
-            if @comment.update(comment_params)
-                render json: {
-                    status: :ok,
-                    successful: ["編集が完了しました。"],
-                    keyword: "update_comment"
                 }
             else
                 render json: {
