8fd2d9ae2b718e860eff655bce6ab1cb368b6789
kasi-iyo <snakeman143@gmail.com>
2020-10-15 23:39:13 +0900

    Applicationコントローラ編集 →new_data_of_series()メソッド内の「小説全件・小説全件の総数・タグ」「お気に入りデータ・お気に入り数」「コメントデータ・コメント数」をそれぞれ個別のメソッドとして抽出 →return_unrelease_data()メソッドを定義 　　→データが非公開の場合に返すレスポンス

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
 # ===============================================================================
 
 # 小説系機能========================================================================
-        helper_method :authorized?, :handle_unauthorized, :release?, :handle_unrelease, :series_and_novels_id,:count_in_series, :new_data_of_series, :all_of_series_data,:set_novel_series, :novels_and_count_of_novels, :data_of_favorites_in_novel, :data_of_comments_in_novel, :one_of_novel_data
+        helper_method :release?, :authorized?, :handle_unauthorized, :set_novel_series,
 
         # ログイン中のユーザーと、今見ているシリーズの作成者が一致するかをbool値で返す
         def authorized?(data)
@@ -55,69 +55,65 @@ class ApplicationController < ActionController::Base
         end
 
     # シリーズのデータを取得するメソッド============================================
+        helper_method :make_new_data_of_series, :return_all_of_series_data
+
         # 新たなシリーズのデータ構造を作成
-        # →all_of_series_data()メソッド, NovelSeriesコントローラのshowアクションにて使用
-        def new_data_of_series(series, dataType)
-            # シリーズに紐付けられた小説
-            novels_and_count_of_novels(series)
-                # 小説が獲得しているお気に入り数をカウント
-                favorites = @novels.map do |novel|
-                    ["favorites_count": novel.novel_favorites.count]
-                end
-                favorites_count = favorites.flatten.sum {|hash| hash[:favorites_count]}
-                # 小説が獲得しているコメント数をカウント
-                comments = @novels.map do |novel|
-                    ["comments_count": novel.comments.count]
-                end
-                comments_count = comments.flatten.sum{|hash| hash[:comments_count]}
-            # シリーズに登録されたタグを取得
-            series_tags = series.novel_tags.map { |tag|
-                ["tag_id": tag.id, "tag_name": tag.novel_tag_name]
-            }.flatten
+        # →return_all_of_series_data()メソッド, NovelSeriesコントローラのshowアクションにて使用
+        def make_new_data_of_series(series, data_type)
+            # シリーズの所有する小説データ(@novels)/小説総数(@novels_count)/登録されているタグ(@series_tags)
+            return_novels_and_novels_count_and_tags(series, true)
+            # 小説が獲得しているお気に入り数(@favorites_count)/コメント数(@comments_count)をカウント
+            return_count_of_favorites_and_comments(@novels)
             # シリーズ全件取得したい場合
-            if dataType === "all_of_series_data"
+            if data_type === "all_of_series_data"
                 return_data_of_series(
-                    data,
-                    novels_count,
-                    favorites_count,
-                    comments_count,
-                    series_tags,
-                    {}
+                    series, @novels_count, @favorites_count, @comments_count, @series_tags, {}
                 )
             # シリーズ1件を取得したい場合
-            elsif dataType === "one_of_series_data"
+            elsif data_type === "one_of_series_data"
                 # 公開の場合
                 # もしくは非公開でもログインユーザーと同じ場合
                 if release?(series)
                     return_data_of_series(
-                        series,
-                        novels_count,
-                        favorites_count,
-                        comments_count,
-                        series_tags,
-                        novels
+                        series, @novels_count, @favorites_count, @comments_count, @series_tags, @novels
                     )
                 elsif !release?(series)
-                    return {
-                        messages: "現在この作品は非公開となっています。"
-                    }
+                    return_unrelease_data()
                 end
             end
         end
 
-        # 1件のシリーズが所有する小説のデータ
-        # →new_data_of_series(), one_of_novel_data()にて使用
-        def novels_and_count_of_novels(series_data)
+        # 1件のシリーズが所有する小説全件のデータ/小説総数/タグを返す
+        # →make_new_data_of_series(), one_of_novel_data()にて使用
+        def return_novels_and_novels_count_and_tags(series_data, data_type)
             # シリーズに紐付けられた小説
             @novels = Novel.where(novel_series_id: series_data.id)
             # 小説全件のカウント
             @novels_count = @novels.count
+            if !!data_type
+                @series_tags = series_data.novel_tags.map { |tag|
+                    ["tag_id": tag.id, "tag_name": tag.novel_tag_name]
+                }.flatten
+            end
+        end
+
+        # 小説が獲得したお気に入り数／コメント数を取得
+        # →make_new_data_of_series()にて使用
+        def return_count_of_favorites_and_comments(novels)
+            count = novels.map do |novel|
+                [
+                    "favorites_count": novel.novel_favorites.count,
+                    "comments_count": novel.comments.count
+                ]
+            end
+            @favorites_count = count.flatten.sum {|hash| hash[:favorites_count]}
+            @comments_count = count.flatten.sum{|hash| hash[:comments_count]}
         end
 
         # returnするシリーズのデータ構造のフォーマット
-        # →new_data()メソッドで使う
+        # →make_new_data_of_series()メソッドで使う
         def return_data_of_series(series, nov_count, fav_count, com_count, tags, novels)
-            return {
+            return  {
                 id: series.id,    # データのID
                 user_id: series.user_id,  # データのユーザーID
                 author: series.author,  # 作者
@@ -134,11 +130,12 @@ class ApplicationController < ActionController::Base
 
         # シリーズデータ全件から新たなデータ構造作成
         # NovelSeriesコントローラのindexにて使用
-        def all_of_series_data(data)
+        def return_all_of_series_data(data)
             data.map do |value|
                 # シリーズが公開されている場合
                 if !!value[:release]
-                    new_data_of_series(value, "all_of_series_data")
+                    # シリーズデータを生成
+                    make_new_data_of_series(value, "all_of_series_data")
                 # シリーズが非公開の場合
                 elsif !value[:release]
                     []
@@ -148,13 +145,15 @@ class ApplicationController < ActionController::Base
     # ==============================================================================
 
     # 小説のデータを取得するメソッド=================================================
+        helper_method :one_of_novel_data
+
         # 新たな小説1件のデータ構造を作成
         # →NovelsControllerのshowアクションにて使用
         def one_of_novel_data(series_data, novel_data)
             # 公開されている場合
             if release?(novel_data)
                 # 小説全件(@novels)／小説総数(@novels_count)
-                novels_and_count_of_novels(series_data)
+                return_novels_and_novels_count_and_tags(series_data, false)
                 # お気に入りをしたユーザーなどのデータ(@favorites_data)／お気に入り数(@favorites_count)
                 data_of_favorites_in_novel(novel_data)
                 # コメントをしたユーザーなどのデータ(@comments_data)／コメント数(@comments_count)
@@ -163,9 +162,8 @@ class ApplicationController < ActionController::Base
                 return_data_of_novel(series_data, novel_data, @novels_count, @favorites_data, @favorites_count, @comments_data, @comments_count)
             # 非公開の場合
             elsif !release?(novel_data)
-                return {
-                    messages: "現在この作品は非公開となっています。"
-                }
+                # 非公開の場合のデータを返す
+                return_unrelease_data()
             end
         end
 
@@ -183,11 +181,11 @@ class ApplicationController < ActionController::Base
                 novel_title: novel_data.novel_title,
                 novel_description: novel_data.novel_description,
                 novel_content: novel_data.novel_content,
-                novels_count: @novels_count,
-                favorites_data: @favorites_data,
-                favorites_count: @favorites_count,
-                comments_data: @comments_data,
-                comments_count: @comments_count,
+                novels_count: novels_count,
+                favorites_data: favorites_data,
+                favorites_count: favorites_count,
+                comments_data: comments_data,
+                comments_count: comments_count,
             }
         end
 
@@ -195,13 +193,21 @@ class ApplicationController < ActionController::Base
         # →one_of_novel_data()メソッドにて使用
         def data_of_favorites_in_novel(novel)
             # お気に入りしたユーザーなど
-            @favorites_data = novel.novel_favorites.map do |favorite|
-                {
-                    favorites_id: favorite.id,
-                    favorites_user_id: favorite.user_id,
-                    favorites_novel_id: favorite.novel_id,
-                    favoriter: favorite.favoriter,
-                }
+            # もしお気に入りされていない場合は、空の配列を返す
+            # これは、React側でfavorites_idによってお気に入りの状態を判別するため
+            if novel.novel_favorites === []
+                @favorites_data = [
+                    favorites_id: "",
+                ]
+            else
+                @favorites_data = novel.novel_favorites.map do |favorite|
+                    {
+                        favorites_id: favorite.id,
+                        favorites_user_id: favorite.user_id,
+                        favorites_novel_id: favorite.novel_id,
+                        favoriter: favorite.favoriter,
+                    }
+                end
             end
             # お気に入り数
             @favorites_count = novel.novel_favorites.count
@@ -225,13 +231,31 @@ class ApplicationController < ActionController::Base
         end
     # ===============================================================================
 
+    # 不正なデータ取得をしてしまった際のレスポンス===================================
+        helper_method :return_not_present_data, :return_unrelease_data
+
         # データが存在しない場合に返すレスポンス
+        # → NovelSeriesコントローラ, Novelsコントローラのprivateにて使用
         def return_not_present_data
+            # React側へ送信するJSONデータ
             render json: {
                 status: 400,
                 errors: "作品が存在しないため、アクセスできません。",
                 keyword: "not_present"
             }
         end
+
+        # データが非公開の場合に返すレスポンス
+        # → make_new_data_of_series(), one_of_novel_data()にて使用
+        def return_unrelease_data
+            # React側へ送信するJSONデータ
+            render json: {
+                status: 200,
+                messages: "現在この作品は非公開となっています。",
+                keyword: "unrelease"
+            }
+        end
+    # ===============================================================================
+
 #====================================================================================
 end
