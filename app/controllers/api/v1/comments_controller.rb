abea460b94e612004cc96c9acce238154f083ce8
kasi-iyo <snakeman143@gmail.com>
2020-10-17 01:38:49 +0900

    index削除 　→シリーズはNovelSeries、小説はNovelコントローラで個別に取得するようにしたため create編集 　→data_of_comments_in_novel()メソッドで、コメント用のデータフォーマットを生成

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,45 +1,33 @@
 class Api::V1::CommentsController < ApplicationController
 
     before_action :logged_in_user, only: [:create, :destroy]
-    before_action :set_novel, only: [:index]
+    before_action :set_novel, only: [:create]
     before_action :set_comment, only: [:destroy]
 
-    def index
-        @comments = @novel.comments
-        render json: {
-            status: 200,
-            comment: @comments,
-            keyword: "index_comments"
-        }
-    end
 
     def create
         @comment = current_user.comments.new(comment_params)
-        if authorized?(@comment)
-            if @comment.save
-                @novel = Novel.find_by(id: params[:novel_id])
-                @comments = @novel.comments
-                render json: {
-                    status: :created,
-                    comments: @comments,
-                    successful: ["正常に送信されました。"],
-                    keyword: "create_comment"
-                }
-            else
-                render json: {
-                    errors: ["入力内容に誤りがあります。"],
-                    status: :unprocessable_entity
-                }
-            end
+        if @comment.save
+            # コメント用のデータフォーマット作成(@comments_data)
+            data_of_comments_in_novel(@novel)
+            render json: {
+                status: :created,
+                comment: @comments_data[0],
+                successful: ["正常に送信されました。"],
+                keyword: "create_comment"
+            }
         else
-            handle_unauthorized(@comment)
+            render json: {
+                errors: ["入力内容に誤りがあります。"],
+                status: :unprocessable_entity
+            }
         end
     end
 
     def destroy
         if authorized?(@comment)
             @comment.destroy
-            render json: { head: :no_content, comment: @comment, success: "正常に削除されました。" }
+            render json: { head: :no_content, success: "正常に削除されました。" }
         else
             handle_unauthorized(@comment)
         end
