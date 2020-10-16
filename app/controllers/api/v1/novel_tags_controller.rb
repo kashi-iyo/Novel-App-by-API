3a61cc7ef3278edaf117329c560db4cdfe91f564
kasi-iyo <snakeman143@gmail.com>
2020-10-17 01:39:55 +0900

    show編集 　→return_all_of_series_data()でタグに関連づけされたシリーズのオブジェクトを取得

--- app/controllers/api/v1/novel_tags_controller.rb
+++ app/controllers/api/v1/novel_tags_controller.rb
@@ -1,42 +1,37 @@
 class Api::V1::NovelTagsController < ApplicationController
 
-    before_action :set_novel_series, only: [:index]
+    before_action :set_tag, only: [:show]
 
     # 小説のタグフィード
     def tags_feed
-        @tags = NovelTag.all
-        @tags.tag_has_series_count(@tags)
+        tags = NovelTag.all
+        # 趣味タグ用のデータを生成
+        @tags = create_new_object(tags, "series_tag")
         render json: {
             status: 200,
             tags: @tags,
             keyword: "tags_feed"}
     end
 
-    # シリーズが所有するタグのデータ
-    def index
-        @id = @novel_series.id.to_s
-        @tags = @novel_series.tags_in_series    #シリーズが所有するタグを取得
-        render json: {
-            status: 200,
-            series_id: @id,
-            series_tags: @tags,
-            keyword: "series_tags"
-        }
-    end
-
     # タグに関連付けられているシリーズ
-    def series_in_tag
-        @tag = NovelTag.find_by(id: params[:id])  # タグ
+    def show
         @series = @tag.novel_series # このタグを持つシリーズ
-        count_in_series(@series)     # シリーズ内にある小説のカウント
-        @series_count = @series.count.to_s  # シリーズのカウント
+        @series_in_tag = return_all_of_series_data(@series)    # タグに関連付けされたシリーズデータを作成
+        @series_count = @series_in_tag.count   #シリーズの総数
         render json: {
             status: 200,
             tag: @tag,
             series_count: @series_count,
-            series_in_tag: @series,
+            series_in_tag: @series_in_tag,
             keyword: "series_in_tag"
         }
     end
+
+    private
+
+        # パラメータに基づいたシリーズを取得
+        def set_tag
+            @tag = NovelTag.find_by(id: params[:id])  # タグ
+        end
 
 end
