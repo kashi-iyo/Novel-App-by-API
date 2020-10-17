f0c2ed24d7f62f7ec66b88cc0fff6380ddd5cce2
kasi-iyo <snakeman143@gmail.com>
2020-10-17 23:57:18 +0900

    create: save_new_object_to_db()メソッドにより、オブジェクトの生成からDBへの保存までを行うようにした edit: get_edit_object()メソッドにより、編集用オブジェクトの取得をできるようにした update: update_object_to_db()メソッドにより、オブジェクトの更新を行うようにした

--- app/controllers/api/v1/novel_series_controller.rb
+++ app/controllers/api/v1/novel_series_controller.rb
@@ -1,98 +1,59 @@
 class Api::V1::NovelSeriesController < ApplicationController
 
-    # ログインしているかどうかの確認
+    # auth ログインしているかどうかの確認
     before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
-    # パラメータの基づいたシリーズ取得
+    # novels パラメータの基づいたシリーズ取得(@novel_series / @errorsを返す)
     before_action :set_novel_series, only: [:show, :edit, :update, :destroy]
-    # パラメータに基づいたタグのデータを取得
+    # novels パラメータに基づいたタグのデータを取得(@novel_tags)
     before_action :set_series_tags, only: [:create, :update]
 
-    # シリーズ全件を取得
+    # novels NovelSeriesオブジェクト全件をJSONとしてレンダリング
     def index
         series = NovelSeries.all
-        all_series = return_all_of_series_data(series)    # ホームに表示させたいシリーズデータを作成
-        series_count = all_series.count   #シリーズの総数
+        @all_series = return_all_series_object(series)    # Applicationコントローラでホームに表示させたいNovelSeriesオブジェクトを取得
+        @series_count = @all_series.count   #取得したNovelSeriesオブジェクトの総数
         render json: {
             status: 200,
-            series_count: series_count,
-            all_series: all_series,
+            series_count: @series_count,
+            all_series: @all_series,
             keyword: "index_of_series"
         }
     end
 
-    # 1つのシリーズ／そのシリーズが持つ小説全件を取得
+    # novels 1つのNovelSeries／そのNovelSeriesが持つNovels全件をJSONとしてレンダリング
     def show
-        series = make_new_data_of_series(@novel_series, "one_of_series_data")
-        render json: {
-            status: 200,
-            series: series,
-            keyword: "show_of_series"
-        }
+        # ApplicationControllerでNovelSeriesのオブジェクトを生成し、そのJSONデータをレンダリングする
+        create_new_series_object(@novel_series, "one_of_series_data")
     end
 
+    # NovelSeriesを作成
     def create
-        # シリーズの作成
         @novel_series = current_user.novel_series.new(novel_series_params)
-        if authorized?(@novel_series)
-            if @novel_series.save
-                # set_series_tagsメソッドに基づきシリーズにタグを登録
-                @novel_series.save_tag(@novel_tags)
-                # React側でリダイレクトに使うシリーズのID
-                series_id = @novel_series.id.to_s
-                render json: {
-                    status: :created,
-                    series_id: series_id,
-                    successful: ["正常に保存されました。"],
-                    keyword: "create_of_series"
-                }
-            else
-                render json: {
-                    errors: @novel_series.errors.full_messages,
-                    status: :unprocessable_entity
-                }
-            end
-        else
-            handle_unauthorized(@novel_series)
-        end
+        # 引数に渡されるデータに基づいて、新規のオブジェクトをDBに保存する
+        save_new_object_to_db(
+            @novel_series,
+            @novel_tags,
+            "series",
+        )
     end
 
+    # NovelSeriesを編集するためのデータを取得
     def edit
-        if authorized?(@novel_series)
-            # 編集用のタグデータを取得
-            @series_tags = @novel_series.series_tags_for_edit
-            render json: {
-                status: 200,
-                novel_series: @novel_series,
-                series_tags: @series_tags,
-                keyword: "edit_of_series" }
-        else
-            handle_unauthorized(@novel_series)
-        end
+        get_edit_object(@novel_series, "series")
     end
 
+    # NovelSeriesを更新
     def update
-        if authorized?(@novel_series)
-            if @novel_series.update(novel_series_params)
-                # set_series_tagsメソッドに基づきシリーズにタグを登録
-                @novel_series.save_tag(@novel_tags)
-                # React側でリダイレクトに使うシリーズのID
-                @series_id = @novel_series.id.to_s
-                render json: {
-                    status: :ok,
-                    series_id: @series_id,
-                    successful: ["編集が完了しました。"],
-                    keyword: "update_of_series"
-                }
-            else
-                render json: {
-                    errors: ["入力内容に誤りがあります。"], status: :unprocessable_entity
-                }
-            end
-        else
-            handle_unauthorized(@novel_series)
-        end
+        # 引数に渡されるデータに基づいて、新規のオブジェクトをDBに保存する
+        update_object_to_db(
+            @novel_series,
+            novel_series_params,
+            @novel_tags,
+            "series",
+        )
     end
 
+    # NovelSeriesを削除
     def destroy
         if authorized?(@novel_series)
             @novel_series.destroy
@@ -104,20 +65,19 @@ class Api::V1::NovelSeriesController < ApplicationController
 
     private
 
-        # シリーズのStrong Parameters
+        # NovelSeriesオブジェクト作成用のStrong Parameters
         def novel_series_params
             params.require(:novel_series).permit(:series_title, :series_description, :author, :release)
         end
 
-        # シリーズ作成時に一緒に送られてくるタグのデータを取得
+        # tags NovelSeriesオブジェクト作成時に一緒に送られてくるNovelTagのデータを取得
         def set_series_tags
             @novel_tags = params[:novel_series][:novel_tag_name].split(",") unless params[:novel_series][:novel_tag_name].nil?
         end
 
-        # シリーズを取得
-        # 取得しようとした時点でデータが削除されている場合があるので、その場合はここで処理
+        # novels パラメータに基づきNovelSeriesオブジェクトを取得
         def set_novel_series
-            # データが存在するかどうかをチェック
+            # validates 欲しいNovelSeriesオブジェクトが存在するかどうかをチェック
             if NovelSeries.find_by(id: params[:id]).nil?
                 return_not_present_data()
             else
