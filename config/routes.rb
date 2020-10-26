b0a482c8ec7a3dbb573df3bc09b97be9e2be6934
kasi-iyo <snakeman143@gmail.com>
2020-10-26 15:07:35 +0900

    Usersコントローラのルーティングをapi, v1で名前空間化

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,17 @@ Rails.application.routes.draw do
   # シリーズ/小説======================================
     namespace :api do
       namespace :v1 do
+        # シリーズ
         resources :novel_series, only: [:index, :show, :edit, :create, :update, :destroy] do
+          # 小説
           resources :novels, only: [:show, :edit, :create, :update, :destroy]
         end
+        # ユーザー
+        resources :users, only: [:create, :show, :edit, :update,  :index]
+        # コメント
+        resources :comments, only: [:create, :destroy]
+        # お気に入り
+        resources :novel_favorites, only: [:create, :destroy]
       end
     end
   # ===================================================
@@ -22,20 +30,20 @@ Rails.application.routes.draw do
   # ==================================================
 
   # コメント/お気に入り================================
-    namespace :api do
-      namespace :v1 do
-        resources :novels do
-          # コメント
-          resources :comments, only: [:create, :destroy]
-          # お気に入り
-          resources :novel_favorites, only: [:create, :destroy]
-        end
-      end
-    end
+    # namespace :api do
+    #   namespace :v1 do
+    #     resources :novels do
+    #       # コメント
+    #       resources :comments, only: [:create, :destroy]
+    #       # お気に入り
+    #       resources :novel_favorites, only: [:create, :destroy]
+    #     end
+    #   end
+    # end
     # =================================================
 
   #ユーザー系====================================
-  resources :users, only: [:create, :show, :edit, :update,  :index]
+  # resources :users, only: [:create, :show, :edit, :update,  :index]
   #=============================================
 
   # 認証============================================
