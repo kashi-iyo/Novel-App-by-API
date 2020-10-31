cadbffbbdda62561c8d8526ecca4f35ce91f7415
kasi-iyo <snakeman143@gmail.com>
2020-10-31 11:26:18 +0900

    ルーティングを整理 →タグ、フォローのルーティングをapi::v1配下に

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,35 @@ Rails.application.routes.draw do
 
   root to: 'api/v1/novel_series#index'
 
-  # シリーズ/小説======================================
-    namespace :api do
-      namespace :v1 do
-        # シリーズ
-        resources :novel_series, only: [:index, :show, :edit, :create, :update, :destroy] do
-          # 小説
-          resources :novels, only: [:show, :edit, :create, :update, :destroy]
-        end
-        # ユーザー
-        resources :users, only: [:create, :show, :edit, :update,  :index]
+  namespace :api do
+    namespace :v1 do
+      # シリーズ
+      resources :novel_series, only: [:index, :show, :edit, :create, :update, :destroy] do
+        # 小説
+        resources :novels, only: [:show, :edit, :create, :update, :destroy]
+      end
+      # ユーザー
+      resources :users, only: [:create, :show, :edit, :update,  :index]
+      # タグ系
+      resources :user_tags, only: [:index, :show]
+      resources :novel_tags, only: [:index, :show]
+      # フォロー
+      resources :relationships, only: [:create, :destroy]
+    end
+  end
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :novels do
         # コメント
         resources :comments, only: [:create, :destroy]
         # お気に入り
         resources :novel_favorites, only: [:create, :destroy]
-        # タグ系
-        resources :user_tags, only: [:index, :show]
-        resources :novel_tags, only: [:index, :show]
       end
     end
-  # ===================================================
+  end
+
 
   # 認証============================================
   post '/login', to: 'sessions#login'
