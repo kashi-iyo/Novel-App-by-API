c936fe08267d8b770761e2db23a0c06ff3e98637
kasi-iyo <snakeman143@gmail.com>
2020-11-04 22:50:44 +0900

    整理

--- app/controllers/concerns/session_concern.rb
+++ app/controllers/concerns/session_concern.rb
@@ -6,7 +6,7 @@ module SessionConcern
         helper_method :pass_object_for_sessions
     end
 
-    # 認証機構
+    #auth 認証機構
     def pass_object_for_sessions(session_data)
         user = session_data[:object]
         params = session_data[:params]
@@ -20,49 +20,61 @@ module SessionConcern
         end
     end
 
-    # ログイン中の挙動
+    #auth ログイン中の挙動
     def logging_case(session_data)
         action = session_data[:action]
-        user = session_data[:user]
         case action
+        #error ログインしているのにログインしようとする
         when "login"
-            return bad_access(message: "すでにログインしています。", action: action)
-        when "is_logged_in?"
-            return return_session_data(user: user, action: action)
+            return unauthorized_errors(errors: "すでにログインしています。")
+        # ログアウトする
         when "logout"
-            return return_session_data(user: "ログアウトしました。", action: action)
+            return return_session_data(action: action)
         end
     end
 
-    # ログインしていない場合の挙動
+    #auth ログインしていない場合の挙動
     def not_logging_case(session_data)
         action = session_data[:action]
         user = session_data[:user]
         params = session_data[:params]
         case action
         when "login"
-            #auth パスワードチェック
+            # パスワードチェック
             if user && user.authenticate(params)
                 login!(user)
-                return return_session_data(user: user, action: action)
+                return return_session_data(
+                    user: {
+                        id: user.id,
+                        nickname: user.nickname,
+                    },
+                    action: action
+                )
+            #error パスワードチェック失敗
             else
-                return bad_access(message: "入力された内容に誤りがあります。", action: action)
+                return unauthorized_errors(errors: "入力された内容に誤りがあります。")
             end
+        #error ログインしてないのにログアウトしようとする場合
         when "logout"
-            return bad_access(message: "不正なアクセスです。", action: action)
+            return unauthorized_errors(errors: "不正なアクセスです。")
         end
     end
 
-    # ユーザーデータをJSONデータで返す
+    #auth ユーザーデータをJSONデータで返す
     def return_session_data(session_data)
         user = session_data[:user]
         action = session_data[:action]
         case action
         when "login"
-            render json: { status: 200, logged_in: true, user: user }
+            render json: {
+                status: 200,
+                logged_in: true,
+                successful: "ログインに成功しました。",
+                object: user
+            }
         when "logout"
             reset_session
-            render json: { status: 200, logged_out: true, user: user }
+            render json: { status: 200, logged_out: true, successful: "ログアウトしました。" }
         end
     end
 
