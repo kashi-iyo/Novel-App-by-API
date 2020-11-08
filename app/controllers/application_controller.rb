41f0a028fe3884842c6c2f6bf39de0d81253851e
kasi-iyo <snakeman143@gmail.com>
2020-11-08 23:15:34 +0900

    no message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,11 +39,12 @@ class ApplicationController < ActionController::Base
         if authorized?(crud_data)
             crud_object(crud_data)
         else
-            case crud_data[:data_type]
+            data_type = crud_data[:data_type]
+            case data_type
             when "relationship"
                 return unauthorized_errors(errors: "自分自身をフォローすることは出来ません。", error_type: "follow_myselfy")
             end
-            return unauthorized_errors(errors: "アクセス権限がありません")
+            return unauthorized_errors(errors: "アクセス権限がありません", error_type: data_type)
         end
     end
 
