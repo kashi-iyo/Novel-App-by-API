4b3360fc8aa4dc41e30268313e532a7ad77ba55d
kasi-iyo <snakeman143@gmail.com>
2020-11-14 21:26:29 +0900

    Userファクトリに、更新用と削除用のユーザーを定義

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,4 +7,16 @@ FactoryBot.define do
     password { "123456" }
     password_confirmation { "123456" }
   end
+
+  factory :updated_user, class: User do
+    nickname { "updated_user" }
+    profile { "updated_profile" }
+    user_tag_name { "" }
+  end
+
+  factory :delete_user, class: User do
+    nickname { "delete_user" }
+    profile { "delete_profile" }
+    user_tag_name { "" }
+  end
 end
