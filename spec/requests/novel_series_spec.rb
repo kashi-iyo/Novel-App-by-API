f022884f681747622e4be6c7dcd1648fd4a94cd7
kasi-iyo <snakeman143@gmail.com>
2020-11-14 21:26:49 +0900

    NovelSeries request specs

--- spec/requests/novel_series_spec.rb
+++ spec/requests/novel_series_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "NovelSeries", type: :request do
-  describe "GET api/v1/novel_series" do
-    context "認証済みのユーザーの場合" do
-      before do
-        @user = FactoryBot.create(:user)
+
+  context "認証済みユーザーの場合" do
+    let :credentials do
+      {email: "authorization@example.com", password: "password", password_confirmation: "password"}
+    end
+
+    let :user do
+      FactoryBot.create(:user, credentials)
+    end
+
+    before do
+      post "/login", params: {user: credentials}
+    end
+
+    describe "GET api/v1/novel_series" do
+      it "200を返すこと" do
+        get root_path
+        expect(response).to be_success
+        expect(response.status).to eq 200
       end
     end
 
-    it "200を返すこと" do
-      get root_path
-      expect(response).to be_success
-      expect(response.status).to eq 200
-    end
   end
+
+
 end
