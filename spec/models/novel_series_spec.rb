18e1921219626f0c5676cb42f2043b64df47fa49
kasi-iyo <snakeman143@gmail.com>
2020-11-15 00:03:27 +0900

    NovelSeries model specs

--- spec/models/novel_series_spec.rb
+++ spec/models/novel_series_spec.rb
@@ -7,4 +7,33 @@ RSpec.describe NovelSeries, type: :model do
     expect(series.novels.length).to eq 5
   end
 
+  it "is タイトルが無ければ無効であること" do
+    series = FactoryBot.build(:novel_series, series_title: nil)
+    series.valid?
+    expect(series.errors[:series_title]).to include("can't be blank")
+  end
+  it "is user_idがない場合無効であること" do
+    series = FactoryBot.build(:novel_series, user_id: nil)
+    series.valid?
+    expect(series.errors[:user_id]).to include("can't be blank")
+  end
+
+  it "is タイトルが50文字を越えた場合無効であること" do
+    series = FactoryBot.build(:novel_series, {series_title: "a" * 51})
+    series.valid?
+    expect(series.errors[:series_title]).to include("is too long (maximum is 50 characters)")
+  end
+  it "is あらすじが300文字を越えた場合無効であること" do
+    series = FactoryBot.build(:novel_series, {series_description: "a" * 301})
+    series.valid?
+    expect(series.errors[:series_description]).to include("is too long (maximum is 300 characters)")
+  end
+  it "is あらすじが300文字を越えた場合無効であること" do
+    series = FactoryBot.build(:novel_series, author: nil)
+    series.valid?
+    expect(series.errors[:author]).to include("can't be blank")
+  end
+
+
+
 end
