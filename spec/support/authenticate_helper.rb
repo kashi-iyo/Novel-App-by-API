4984d21cd464f387b2dca5c6e672a6c45e99b9b3
kasi-iyo <snakeman143@gmail.com>
2020-11-14 18:27:12 +0900

    テスト用　認証系ヘルパー

---
+++ spec/support/authenticate_helper.rb
@@ -0,0 +1,24 @@
+module AuthenticateHelper
+
+    # extend ActiveSupport::Concern
+
+    def is_logged_in?
+        # !cookies[:user_id].nil?
+        !session[:user_id].nil?
+    end
+
+    # ログインさせる
+    def login!(user)
+        session[:user_id] = user.id
+    end
+
+    # ログインしているかどうかをbool値で返す
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    # 現在ログインしているユーザーを返す
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if logged_in?
+    end
+end
