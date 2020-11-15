998e2274e3b53eb8dbca7cc5a78c3efe934d1cd0
kasi-iyo <snakeman143@gmail.com>
2020-11-16 01:37:03 +0900

    Sessions request specs 追加

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,57 +2,122 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
 
-  before do
-    @credentials = {
-      email: "authorization@example.com",
-      password: "password",
-      password_confirmation: "password"}
-    @user = FactoryBot.create(:user, @credentials)
-    post "/login", params: {user: @credentials}
-  end
-
-  describe "GET /login" do
-    it "200を返す" do
-      expect(response).to have_http_status(200)
-    end
-    it "正しいJSONレスポンスを返す" do
-      json = JSON.parse(response.body)
-      expect("正常にログイン出来ました。").to eq json["successful"]
-    end
-    it "ログイン状態がtrueであること" do
-      expect(is_logged_in?).to be_truthy
-    end
-  end
-
-  describe "GET /logged_in" do
+  context "未ログインの場合" do
     before do
-      get "/logged_in", params: {user: @credentials}
-    end
-    it "200を返す" do
-      expect(response).to have_http_status(200)
-    end
-    it "正しいJSONレスポンスを返す" do
-      json = JSON.parse(response.body)
-      expect(true).to eq json["logged_in"]
-    end
-    it "ログイン状態がtrueであること" do
-      expect(is_logged_in?).to be_truthy
-    end
-  end
-
-  describe "DELETE /logout" do
-    before do
+      @credentials = {
+        email: "authorization@example.com",
+        password: "password",
+        password_confirmation: "password"}
+      @user = FactoryBot.create(:user, @credentials)
       delete "/logout"
     end
-    it "200を返す" do
-      expect(response).to have_http_status(200)
+
+    describe "GET /login" do
+      before do
+        post "/login", params: {user: @credentials}
+      end
+      it "200を返すこと" do
+        expect(response).to have_http_status(200)
+      end
+      it "正しいJSONレスポンスを返すこと" do
+        json = JSON.parse(response.body)
+        expect("正常にログイン出来ました。").to eq json["successful"]
+      end
+      it "ログイン状態がtrueであること" do
+        expect(is_logged_in?).to be_truthy
+      end
     end
-    it "正しいJSONレスポンスを返す" do
-      json = JSON.parse(response.body)
-      expect(false).to eq json["logged_in"]
+
+    describe "GET /logged_in" do
+      before do
+        get "/logged_in", params: {user: @credentials}
+      end
+      it "200を返すこと" do
+        expect(response).to have_http_status(200)
+      end
+      it "正しいJSONレスポンスを返すこと" do
+        json = JSON.parse(response.body)
+        expect(false).to eq json["logged_in"]
+      end
+      it "ログイン状態がfalseであること" do
+        expect(is_logged_in?).to be_falsey
+      end
     end
-    it "ログイン状態がtrueであること" do
-      expect(is_logged_in?).to be_falsey
+
+    describe "DELETE /logout" do
+      before do
+        delete "/logout"
+      end
+      it "200を返すこと" do
+        expect(response).to have_http_status(200)
+      end
+      it "正しいJSONレスポンスを返すこと" do
+        json = JSON.parse(response.body)
+        expect("不正なアクセスです。").to eq json["errors"]
+      end
+      it "ログイン状態がfalseであること" do
+        expect(is_logged_in?).to be_falsey
+      end
     end
   end
+
+  context "ログインしている場合" do
+    before do
+      @credentials = {
+        email: "authorization@example.com",
+        password: "password",
+        password_confirmation: "password"}
+      @user = FactoryBot.create(:user, @credentials)
+      post "/login", params: {user: @credentials}
+    end
+
+    describe "GET /login" do
+      before do
+        post "/login", params: {user: @credentials}
+      end
+      it "200を返すこと" do
+        expect(response).to have_http_status(200)
+      end
+      it "正しいJSONレスポンスを返すこと" do
+        json = JSON.parse(response.body)
+        expect("すでにログインしています。").to eq json["errors"]
+      end
+      it "ログイン状態がtrueであること" do
+        expect(is_logged_in?).to be_truthy
+      end
+    end
+
+    describe "GET /logged_in" do
+      before do
+        get "/logged_in", params: {user: @credentials}
+      end
+      it "200を返すこと" do
+        expect(response).to have_http_status(200)
+      end
+      it "正しいJSONレスポンスを返すこと" do
+        json = JSON.parse(response.body)
+        expect(true).to eq json["logged_in"]
+      end
+      it "ログイン状態がtrueであること" do
+        expect(is_logged_in?).to be_truthy
+      end
+    end
+
+    describe "DELETE /logout" do
+      before do
+        delete "/logout"
+      end
+      it "200を返すこと" do
+        expect(response).to have_http_status(200)
+      end
+      it "正しいJSONレスポンスを返すこと" do
+        json = JSON.parse(response.body)
+        expect(false).to eq json["logged_in"]
+      end
+      it "ログイン状態がfalseであること" do
+        expect(is_logged_in?).to be_falsey
+      end
+    end
+  end
+
 end
