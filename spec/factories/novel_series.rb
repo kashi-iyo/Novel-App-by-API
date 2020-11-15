685d41135d9e1f7f0bcde20a28386085230d040b
kasi-iyo <snakeman143@gmail.com>
2020-11-16 01:36:32 +0900

    NovelSeriesのファクトリを追加

--- spec/factories/novel_series.rb
+++ spec/factories/novel_series.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     series_title { "タイトル" }
     series_description { "あらすじ" }
     release { false }
-    author { "田中" }
     owner
+    author { owner.nickname }
 
     # 公開されているシリーズ
     trait :is_release do
@@ -15,6 +15,20 @@ FactoryBot.define do
     trait :has_novels do
       after(:create) { |series| create_list(:novel, 5, novel_series: series) }
     end
+  end
+
+  factory :updated_series, class: NovelSeries do
+    series_title { "更新用シリーズタイトル" }
+    release { true }
+    owner
+    author { owner.nickname }
+  end
+
+  factory :delete_series, class: NovelSeries do
+    series_title { "削除用シリーズタイトル" }
+    release { true }
+    owner
+    author { owner.nickname }
   end
 
 end
