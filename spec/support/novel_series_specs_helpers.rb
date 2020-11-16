2c883618543243e621c0cb10c207ef6349ad6971
kasi-iyo <snakeman143@gmail.com>
2020-11-16 16:57:45 +0900

    NovelSeries用のヘルパーを作成

--- spec/support/novel_series_specs_helpers.rb
+++ spec/support/novel_series_specs_helpers.rb
@@ -12,4 +12,26 @@ module NovelSeriesSpecsHelpers
         delete "/api/v1/novel_series/#{series_params[:id]}"
     end
 
+    # シリーズにお気に入りやコメントなどのデータを追加する
+    def return_series_having_favorites(pass_data)
+        # データが追加される小説に紐付けさせるためのシリーズを作成
+        @series_having_items = FactoryBot.create_list(
+            :novel_series, pass_data[:count], :is_release)
+        # データが追加される小説を作成
+        novel = @series_having_items.map do |series|
+            FactoryBot.create(:novel, novel_series: series)
+        end
+        # データの追加を行うユーザーを作成
+        user = FactoryBot.create(:user)
+        # データの追加を実行
+        novel.map do |val|
+            case pass_data[:type]
+            when "favorites"
+                FactoryBot.create(:novel_favorite, user: user, novel: val)
+            when "comments"
+                FactoryBot.create(:comment, user: user, novel: val)
+            end
+        end
+    end
+
 end
