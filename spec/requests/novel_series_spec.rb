c9e32ead2f314cdaf55b83224b68cc5d17feb50b
kasi-iyo <snakeman143@gmail.com>
2020-11-18 17:11:07 +0900

    NovelSeries request specにspecを追加

--- spec/requests/novel_series_spec.rb
+++ spec/requests/novel_series_spec.rb
@@ -5,37 +5,83 @@ RSpec.describe "NovelSeries", type: :request do
   # シリーズ全件（認証有無関係ない）
   describe "GET api/v1/novel_series" do
     context "シリーズが公開されている場合" do
-      before do
-        @series = FactoryBot.create_list(:novel_series, 5, :is_release)
-        get root_path
+      context "何も付与されていないシリーズを返す場合" do
+        before do
+          # 公開シリーズを5つ作成
+          @series = FactoryBot.create_list(:novel_series, 5, :is_release)
+          get root_path
+        end
+        it "200を返すこと" do
+          expect(response.status).to eq 200
+        end
+        it "5つのシリーズを返すこと" do
+          expect_series_length(response, 5)
+        end
       end
-      it "200を返すこと" do
-        expect(response.status).to eq 200
+      context "シリーズにお気に入りがある場合" do
+        before do
+          return_series_having_favorites({count: 5, type: "favorites"})
+          get root_path
+        end
+        it "200を返すこと" do
+          expect(response.status).to eq 200
+        end
+        it "5つのシリーズを返すこと" do
+          expect_series_length(response, 5)
+        end
+        it "前提として小説を持つこと" do
+          expect_series_include_items(response, "novels_count")
+        end
+        it "5つのそれぞれのシリーズが1件のお気に入りを持つこと" do
+          expect_series_include_items(response, "favorites_count")
+        end
       end
-      it "5つのシリーズを返すこと" do
-        json = JSON.parse(response.body)
-        expect(json["object"]["series_count"]).to eq 5
+      context "シリーズにコメントがある場合" do
+        before do
+          return_series_having_favorites({count: 5, type: "comments"})
+          get root_path
+        end
+        it "200を返すこと" do
+          expect(response.status).to eq 200
+        end
+        it "5つのシリーズを返すこと" do
+          expect_series_length(response, 5)
+        end
+        it "前提として小説を持つこと" do
+          expect_series_include_items(response, "novels_count")
+        end
+        it "5つのそれぞれのシリーズが1件のお気に入りを持つこと" do
+          expect_series_include_items(response, "comments_count")
+        end
       end
-      it "シリーズの配列数が5つであること" do
-        json = JSON.parse(response.body)
-        expect(json["object"]["series"].length).to eq 5
+      context "シリーズにタグがある場合" do
+        before do
+          # 1件のタグ、5件のシリーズを作成 & 左記を関連付け
+          return_one_tag_and_multiple_series()
+          get root_path
+        end
+        it "200を返すこと" do
+          expect(response.status).to eq 200
+        end
+        it "5つのシリーズを返すこと" do
+          expect_series_length(response, 5)
+        end
+        it "5つのそれぞれのシリーズが1件のタグを持つこと" do
+          expect_multiple_series_length(response, 1, "tags")
+        end
       end
     end
     context "シリーズが非公開の場合" do
       before do
+        # 非公開シリーズを作成
         @series = FactoryBot.create_list(:novel_series, 5)
         get root_path
       end
       it "200を返すこと" do
         expect(response.status).to eq 200
       end
-      it "返すシリーズが0であること" do
-        json = JSON.parse(response.body)
-        expect(json["object"]["series_count"]).to eq 0
-      end
-      it "シリーズの配列数が0であること" do
-        json = JSON.parse(response.body)
-        expect(json["object"]["series"].length).to eq 0
+      it "返すシリーズがないこと" do
+        expect_series_length(response, 0)
       end
     end
   end
@@ -54,12 +100,10 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "6つのシリーズを返すこと" do
-          json = JSON.parse(response.body)
-          expect(json["object"]["series_count"]).to eq 6
+          expect_series_length(response, 6)
         end
         it "最初に最新のシリーズが来ること" do
-          json = JSON.parse(response.body)["object"]["series"].first
-          expect(@new_series.id).to eq json["series"]["id"]
+          expect_series_order(response, @new_series.id)
         end
       end
       context "古い順になっている場合" do
@@ -73,12 +117,10 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "6つのシリーズを返すこと" do
-          json = JSON.parse(response.body)
-          expect(json["object"]["series_count"]).to eq 6
+          expect_series_length(response, 6)
         end
         it "最初に古いシリーズが来ること" do
-          json = JSON.parse(response.body)["object"]["series"].first
-          expect(@old_series.id).to eq json["series"]["id"]
+          expect_series_order(response, @old_series.id)
         end
       end
       context "お気に入りが多い順になっている場合" do
@@ -92,12 +134,10 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "6つのシリーズを返すこと" do
-          json = JSON.parse(response.body)
-          expect(json["object"]["series_count"]).to eq 6
+          expect_series_length(response, 6)
         end
         it "最初にお気に入りが多いシリーズが来ること" do
-          json = JSON.parse(response.body)["object"]["series"].first
-          expect(@series_having_items[0].id).to eq json["series"]["id"]
+          expect_series_order(response, @series_having_items[0].id)
         end
       end
       context "お気に入りが少ない順になっている場合" do
@@ -111,12 +151,10 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "6つのシリーズを返すこと" do
-          json = JSON.parse(response.body)
-          expect(json["object"]["series_count"]).to eq 6
+          expect_series_length(response, 6)
         end
         it "最初にお気に入りが多いシリーズが来ること" do
-          json = JSON.parse(response.body)["object"]["series"].first
-          expect(@series.id).to eq json["series"]["id"]
+          expect_series_order(response, @series.id)
         end
       end
       context "コメントが多い順になっている場合" do
@@ -130,12 +168,10 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "6つのシリーズを返すこと" do
-          json = JSON.parse(response.body)
-          expect(json["object"]["series_count"]).to eq 6
+          expect_series_length(response, 6)
         end
         it "最初にコメントが多いシリーズが来ること" do
-          json = JSON.parse(response.body)["object"]["series"].first
-          expect(@series_having_items[0].id).to eq json["series"]["id"]
+          expect_series_order(response, @series_having_items[0].id)
         end
       end
       context "コメントが少ない順になっている場合" do
@@ -149,12 +185,10 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "6つのシリーズを返すこと" do
-          json = JSON.parse(response.body)
-          expect(json["object"]["series_count"]).to eq 6
+          expect_series_length(response, 6)
         end
         it "最初にコメントが少ないシリーズが来ること" do
-          json = JSON.parse(response.body)["object"]["series"].first
-          expect(@series.id).to eq json["series"]["id"]
+          expect_series_order(response, @series.id)
         end
       end
     end
@@ -167,12 +201,7 @@ RSpec.describe "NovelSeries", type: :request do
         expect(response.status).to eq 200
       end
       it "返すシリーズが0であること" do
-        json = JSON.parse(response.body)
-        expect(json["object"]["series_count"]).to eq 0
-      end
-      it "シリーズの配列数が0であること" do
-        json = JSON.parse(response.body)
-        expect(json["object"]["series"].length).to eq 0
+        expect_series_length(response, 0)
       end
     end
   end
@@ -194,8 +223,7 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "正しいJSONレスポンスを返すこと" do
-          json = JSON.parse(response.body)
-          expect("公開シリーズ").to eq json["object"]["series"]["series_title"]
+          expect_series_value(response, @series.series_title, "series_title")
         end
       end
       context "シリーズが非公開の場合" do
@@ -205,7 +233,6 @@ RSpec.describe "NovelSeries", type: :request do
           # 異なるユーザーのシリーズ
           @other_user_series = FactoryBot.create(:novel_series)
         end
-
         context "ログインユーザー自身の非公開作品の場合" do
           before do
             # 自身のシリーズへのアクセス
@@ -215,11 +242,9 @@ RSpec.describe "NovelSeries", type: :request do
             expect(response.status).to eq 200
           end
           it "正しいJSONレスポンスを返すこと" do
-            json = JSON.parse(response.body)
-            expect("ログインユーザー").to eq json["object"]["series"]["author"]
+            expect_series_value(response, @series.author, "author")
           end
         end
-
         context "異なるユーザーの非公開作品の場合" do
           before do
             # 異なるユーザーの非公開シリーズへのアクセス
@@ -228,30 +253,61 @@ RSpec.describe "NovelSeries", type: :request do
           it "200を返すこと" do
             expect(response.status).to eq 200
           end
-          it "不正なJSONレスポンスを返すこと" do
-            json = JSON.parse(response.body)
-            expect("アクセス権限がありません。").to eq json["errors"]
+          it "アクセス権限がないこと" do
+            expect_can_not_access(response)
           end
         end
-
       end
     end
 
     # シリーズ作成
     describe "POST api/v1/novel_series/" do
-      before do
-        @params = FactoryBot.attributes_for(:novel_series, owner: @user, novel_tag_name: "")
-        request_post(@params)
+      context "作成に成功する場合" do
+        before do
+          @params = FactoryBot.attributes_for(:novel_series, owner: @user, novel_tag_name: "")
+          request_post(@params)
+        end
+        it "200を返すこと" do
+          expect(response.status).to eq 200
+        end
+        it "正しいJSONレスポンスを返すこと" do
+          expect_save_ok(response)
+        end
+        it "シリーズがDBに追加されていること" do
+          expect{request_post(@params)}.to change(NovelSeries, :count).by(1)
+        end
       end
-      it "200を返すこと" do
-        expect(response.status).to eq 200
-      end
-      it "正しいJSONレスポンスを返すこと" do
-        json = JSON.parse(response.body)
-        expect("正常に保存されました。").to eq json["successful"]
-      end
-      it "シリーズがDBに追加されていること" do
-        expect{request_post(@params)}.to change(NovelSeries, :count).by(1)
+      context "作成に失敗する場合" do
+        context "タイトルが入力されていない場合" do
+          before do
+            @params = FactoryBot.attributes_for(:novel_series, series_title: nil, owner: @user, novel_tag_name: "")
+            request_post(@params)
+          end
+          it "200を返すこと" do
+            expect(response.status).to eq 200
+          end
+          it "エラーレスポンスを返すこと" do
+            expect_can_not_be_blank(response, "Series title")
+          end
+          it "シリーズがDBに追加されないこと" do
+            expect{request_post(@params)}.to change(NovelSeries, :count).by(0)
+          end
+        end
+        context "作者がパラメータに存在しない場合" do
+          before do
+            @params = FactoryBot.attributes_for(:novel_series, owner: nil, author: nil, novel_tag_name: "")
+            request_post(@params)
+          end
+          it "200を返すこと" do
+            expect(response.status).to eq 200
+          end
+          it "エラーレスポンスを返すこと" do
+            expect_can_not_be_blank(response, "Author")
+          end
+          it "シリーズがDBに追加されないこと" do
+            expect{request_post(@params)}.to change(NovelSeries, :count).by(0)
+          end
+        end
       end
     end
 
@@ -272,8 +328,7 @@ RSpec.describe "NovelSeries", type: :request do
             expect(response.status).to eq 200
           end
           it "正しいJSONレスポンスを返すこと" do
-            json = JSON.parse(response.body)
-            expect("公開シリーズ").to eq json["object"]["series_title"]
+            expect_object_value(response, @series.series_title, "series_title")
           end
         end
         context "非公開の場合" do
@@ -289,12 +344,10 @@ RSpec.describe "NovelSeries", type: :request do
             expect(response.status).to eq 200
           end
           it "正しいJSONレスポンスを返すこと" do
-            json = JSON.parse(response.body)
-            expect("非公開シリーズ").to eq json["object"]["series_title"]
+            expect_object_value(response, @series.series_title, "series_title")
           end
         end
       end
-
       context "異なるユーザーの作品の場合" do
         before do
           @other_user = FactoryBot.create(:user)
@@ -308,47 +361,72 @@ RSpec.describe "NovelSeries", type: :request do
         it "200を返すこと" do
           expect(response.status).to eq 200
         end
-        it "不正なJSONレスポンスを返すこと" do
-          json = JSON.parse(response.body)
-          expect("アクセス権限がありません。").to eq json["errors"]
+        it "アクセス権限がないこと" do
+          expect_can_not_access(response)
         end
       end
     end
 
     # シリーズ更新
     describe "PUT api/v1/novel_series/:id" do
-      before do
-        @updated_params = FactoryBot.attributes_for(:updated_series, owner: @user, novel_tag_name: "")
-      end
       context "ログインユーザー自身の作品の場合" do
-        before do
-          @before_series = FactoryBot.create(
-            :novel_series,
-            owner: @user,
-          )
-          request_update({id: @before_series.id, params: @updated_params})
+        context "更新に成功する場合" do
+          before do
+            @before_series = FactoryBot.create(:novel_series, owner: @user)
+            @updated_params = FactoryBot.attributes_for(:updated_series, owner: @user, novel_tag_name: "")
+            request_update({id: @before_series.id, params: @updated_params})
+          end
+          it "200を返すこと" do
+            expect(response.status).to eq 200
+          end
+          it "正しいJSONレスポンスを返すこと" do
+            expect_update_ok(response)
+          end
         end
-        it "200を返すこと" do
-          expect(response.status).to eq 200
-        end
-        it "正しいJSONレスポンスを返すこと" do
-          json = JSON.parse(response.body)
-          expect("正常に編集が完了しました。").to eq json["successful"]
+        context "更新に失敗する場合" do
+          context "タイトルが入力されていない場合" do
+            before do
+              @before_series = FactoryBot.create(:novel_series, owner: @user)
+              @updated_params = FactoryBot.attributes_for(
+                :updated_series, owner: @user, series_title: nil,  novel_tag_name: "")
+              request_update({id: @before_series.id, params: @updated_params})
+            end
+            it "200を返すこと" do
+              expect(response.status).to eq 200
+            end
+            it "エラーレスポンスを返すこと" do
+              expect_can_not_be_blank(response, "Series title")
+            end
+          end
+          context "作者がパラメータに含まれていない場合" do
+            before do
+              @before_series = FactoryBot.create(:novel_series, owner: @user)
+              @updated_params = FactoryBot.attributes_for(:updated_series, owner: @user, author:nil, novel_tag_name: "")
+              request_update({id: @before_series.id, params: @updated_params})
+            end
+            it "200を返すこと" do
+              expect(response.status).to eq 200
+              puts "#{response.body}"
+            end
+            it "エラーレスポンスを返すこと" do
+              expect_can_not_be_blank(response, "Author")
+            end
+          end
         end
       end
-
       context "異なるユーザーの作品の場合" do
         before do
           @other_user = FactoryBot.create(:user)
           @other_users_series = FactoryBot.create(:updated_series, owner: @other_user)
+          @updated_params = FactoryBot.attributes_for(
+            :updated_series, owner: @user, novel_tag_name: "")
           request_update({id: @other_users_series.id, params: @updated_params})
         end
         it "200を返すこと" do
           expect(response.status).to eq 200
         end
         it "不正なJSONレスポンスを返すこと" do
-          json = JSON.parse(response.body)
-          expect("アクセス権限がありません。").to eq json["errors"]
+          expect_can_not_access(response)
         end
       end
     end
@@ -364,8 +442,7 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "正しいJSONレスポンスを返すこと" do
-          json = JSON.parse(response.body)
-          expect("正常に削除されました。").to eq json["successful"]
+          expect_delete_ok(response)
         end
       end
 
@@ -379,8 +456,7 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "不正なJSONレスポンスを返すこと" do
-          json = JSON.parse(response.body)
-          expect("アクセス権限がありません。").to eq json["errors"]
+          expect_can_not_access(response)
         end
       end
     end
@@ -403,8 +479,7 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "正しいJSONレスポンスを返すこと" do
-          json = JSON.parse(response.body)
-          expect("公開シリーズ").to eq json["object"]["series"]["series_title"]
+          expect_series_value(response, @series.series_title, "series_title")
         end
       end
       context "シリーズが非公開の場合" do
@@ -416,8 +491,7 @@ RSpec.describe "NovelSeries", type: :request do
           expect(response.status).to eq 200
         end
         it "不正なJSONレスポンスを返すこと" do
-          json = JSON.parse(response.body)
-          expect("アクセス権限がありません。").to eq json["errors"]
+          expect_can_not_access(response)
         end
       end
     end
@@ -432,8 +506,7 @@ RSpec.describe "NovelSeries", type: :request do
         expect(response.status).to eq 200
       end
       it "正しいJSONレスポンスを返すこと" do
-        json = JSON.parse(response.body)
-        expect("この機能を使用するにはログインまたは、新規登録が必要です。").to eq json["errors"]
+        expect_need_auth(response)
       end
       it "シリーズがDBに追加されないこと" do
         expect{request_post(@params)}.to change(NovelSeries, :count).by(0)
@@ -454,8 +527,7 @@ RSpec.describe "NovelSeries", type: :request do
         expect(response.status).to eq 200
       end
       it "不正なJSONレスポンスを返すこと" do
-        json = JSON.parse(response.body)
-        expect("この機能を使用するにはログインまたは、新規登録が必要です。").to eq json["errors"]
+        expect_need_auth(response)
       end
     end
 
@@ -473,8 +545,7 @@ RSpec.describe "NovelSeries", type: :request do
         expect(response.status).to eq 200
       end
       it "正しいJSONレスポンスを返すこと" do
-        json = JSON.parse(response.body)
-        expect("この機能を使用するにはログインまたは、新規登録が必要です。").to eq json["errors"]
+        expect_need_auth(response)
       end
     end
 
@@ -488,8 +559,7 @@ RSpec.describe "NovelSeries", type: :request do
         expect(response.status).to eq 200
       end
       it "不正なJSONレスポンスを返すこと" do
-        json = JSON.parse(response.body)
-        expect("この機能を使用するにはログインまたは、新規登録が必要です。").to eq json["errors"]
+        expect_need_auth(response)
       end
     end
   end
