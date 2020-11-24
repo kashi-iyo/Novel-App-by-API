94597eda86377293044d749a42e3efdd76c46cc0
kasi-iyo <snakeman143@gmail.com>
2020-11-25 02:02:07 +0900

    no message

--- scripts/bundle_install.sh
+++ scripts/bundle_install.sh
@@ -5,6 +5,7 @@ source /etc/environment
 echo $str1$DB_NAME
 cd /var/www/rails/Novel-App-by-API
 echo $str1$DB_NAME
-RAILS_ENV=production bundle
-RAILS_ENV=production bundle exec rake db:migrate
-RAILS_ENV=production bundle exec rake assets:precompile
+bundle -v
+# RAILS_ENV=production bundle
+# RAILS_ENV=production bundle exec rake db:migrate
+# RAILS_ENV=production bundle exec rake assets:precompile
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+
+# cd /var/www/rails/Novel-App-by-API
 # sudo systemctl restart nginx
-cd /var/www/rails/Novel-App-by-API
-sudo systemctl restart nginx
-bundle exec unicorn_rails -c /var/www/rails/Novel-App-by-API/config/unicorn.conf.rb -D -E production
+# bundle exec unicorn_rails -c /var/www/rails/Novel-App-by-API/config/unicorn.conf.rb -D -E production
