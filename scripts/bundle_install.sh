1cd844d906a7001dccbcde4455e01b6ee57aa772
kasi-iyo <snakeman143@gmail.com>
2020-11-25 02:13:12 +0900

    no message

--- scripts/bundle_install.sh
+++ scripts/bundle_install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 str1="データベース名"
+
 source /etc/environment
+source ~/.bash_profile
+
 echo $str1$DB_NAME
 cd /var/www/rails/Novel-App-by-API
-echo $str1$DB_NAME
 bundle -v
+
 # RAILS_ENV=production bundle
 # RAILS_ENV=production bundle exec rake db:migrate
 # RAILS_ENV=production bundle exec rake assets:precompile
