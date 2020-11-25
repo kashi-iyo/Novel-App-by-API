138d0ccd94c498b17db690e70394acdd46185f85
kasi-iyo <snakeman143@gmail.com>
2020-11-25 20:43:02 +0900

    no message

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# ec2-userへ所有権を渡す
+sudo chown -R ec2-user /var/www/rails/Novel-App-by-API
+
+# アプリのディレクトリへ移動
 cd /var/www/rails/Novel-App-by-API
+
+# Nginxの起動
+sudo systemctl restart nginx
 
 # bundleコマンドを反映
 source ~/.bash_profile
 
-sudo chown -R ec2-user /var/www/rails/Novel-App-by-API
+bundler -v
+echo $RAILS_MASTER_KEY
 
-sudo systemctl restart nginx
+# Unicornの起動
 bundle exec unicorn_rails -c /var/www/rails/Novel-App-by-API/config/unicorn.conf.rb -D -E production
