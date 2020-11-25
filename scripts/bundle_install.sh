a9cdd31f4eb743821f4de6c65f49b047240400c6
kasi-iyo <snakeman143@gmail.com>
2020-11-25 17:23:44 +0900

    no message

--- scripts/bundle_install.sh
+++ scripts/bundle_install.sh
@@ -8,6 +8,8 @@ source ~/.bash_profile
 
 cd /var/www/rails/Novel-App-by-API
 
+sudo chown -R ec2-user /var/www/rails/Novel-App-by-API
+
 RAILS_ENV=production bundle
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production bundle exec rake assets:precompile
