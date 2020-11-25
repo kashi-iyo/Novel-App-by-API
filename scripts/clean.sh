b9ef3d20263281cb7ce8073e4c91fbe94c3064dd
kasi-iyo <snakeman143@gmail.com>
2020-11-25 20:54:34 +0900

    no message

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo $RAILS_MASTER_KEY
 sudo systemctl stop nginx
 kill -KILL -s QUIT `cat /var/www/rails/Novel-App-by-API/tmp/pids/unicorn.pid`
 sudo rm -rf /var/www/rails/Novel-App-by-API
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $RAILS_MASTER_KEY
+
 # master.keyの環境変数を反映
 source /etc/environment
 
@@ -10,6 +12,13 @@ cd /var/www/rails/Novel-App-by-API
 
 sudo chown -R ec2-user /var/www/rails/Novel-App-by-API
 
+# Railsアプリをproductionでセットアップ
 RAILS_ENV=production bundle
 RAILS_ENV=production bundle exec rake db:create db:migrate
 RAILS_ENV=production bundle exec rake assets:precompile
+
+# Nginxの起動
+sudo systemctl restart nginx
+
+# Unicornの起動
+bundle exec unicorn_rails -c /var/www/rails/Novel-App-by-API/config/unicorn.conf.rb -D -E production
