3ec9469b7400f00175635af204ad16aabd61bafb
kasi-iyo <snakeman143@gmail.com>
2020-11-28 21:33:20 +0900

    no message

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_novel-app-api', domain: :all
+    Rails.application.config.session_store :cookie_store, key: '_Novel-App-by-API', domain: :all
 else
     Rails.application.config.session_store :cookie_store, key: '_novel-app-api'
 end
