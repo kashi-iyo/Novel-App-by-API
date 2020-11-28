b6b8b36b0b9108627e74a6836489851c6f043bb2
kasi-iyo <snakeman143@gmail.com>
2020-11-29 00:17:28 +0900

    no message

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,16 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#     allow do
+#         origins 'http://localhost:3000', 'http://novel-app-client.s3-website-ap-northeast-1.amazonaws.com'
+
+#         resource '*',
+#             headers: :any,
+#             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#             credentials: true
+#     end
+# end
+Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
     allow do
-        origins 'http://localhost:3000', 'http://novel-app-client.s3-website-ap-northeast-1.amazonaws.com'
+        origins 'http://novel-app-client.s3-website-ap-northeast-1.amazonaws.com'
 
         resource '*',
             headers: :any,
