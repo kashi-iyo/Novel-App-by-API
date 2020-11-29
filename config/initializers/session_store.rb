c1e3946282d86c06b66890c1685380b77104647c
kasi-iyo <snakeman143@gmail.com>
2020-11-30 00:36:05 +0900

    no message

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_novel-app-api', domain: 'http://54.65.39.121', expire_after: 20.years
+    Rails.application.config.session_store :cookie_store, key: '_novel-app-api', domain: '54.65.39.121', expire_after: 20.years
 else
     Rails.application.config.session_store :cookie_store, key: '_novel-app-api'
 end
