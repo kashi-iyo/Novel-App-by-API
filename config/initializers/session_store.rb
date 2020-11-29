a9299f7302a3064a68fefd492fb1d572e456ce1b
kasi-iyo <snakeman143@gmail.com>
2020-11-29 22:42:54 +0900

    no message

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_novel-app-api', expire_after: 20.years
+    Rails.application.config.session_store :cookie_store, key: '_novel-app-api', domain: 'http://d1josdpzepprr2.cloudfront.net', expire_after: 20.years
 else
     Rails.application.config.session_store :cookie_store, key: '_novel-app-api'
 end
